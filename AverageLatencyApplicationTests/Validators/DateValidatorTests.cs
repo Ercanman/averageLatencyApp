a93a56dc822aa25e22e99646140cc5543df3f98f
EricvB <eric.vanbeers1@gmail.com>
2025-10-01 17:46:30 +0200

    Add additional unit tests for validators and services

--- AverageLatencyApplicationTests/Validators/DateValidatorTests.cs
+++ AverageLatencyApplicationTests/Validators/DateValidatorTests.cs
@@ -24,5 +24,45 @@ namespace AverageLatencyApplicationTests.Validators
             result.ShouldHaveValidationErrorFor(d => d.StartDate);
             result.ShouldHaveValidationErrorFor(d => d.EndDate);
         }
+
+        [Fact]
+        public void ValidDates_Should_PassValidation()
+        {
+            var input = new DateDto("2021-01-01", "2021-01-10");
+
+            var result = _validator.TestValidate(input);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void StartDateBeforeAllowedRange_Should_NotBeAllowed()
+        {
+            var input = new DateDto("2020-12-31", "2021-01-02");
+
+            var result = _validator.TestValidate(input);
+
+            result.ShouldHaveValidationErrorFor(d => d.StartDate);
+        }
+
+        [Fact]
+        public void EndDateAfterAllowedRange_Should_NotBeAllowed()
+        {
+            var input = new DateDto("2021-12-30", "2022-01-01");
+
+            var result = _validator.TestValidate(input);
+
+            result.ShouldHaveValidationErrorFor(d => d.EndDate);
+        }
+
+        [Fact]
+        public void EndDateBeforeStartDate_Should_NotBeAllowed()
+        {
+            var input = new DateDto("2021-01-10", "2021-01-05");
+
+            var result = _validator.TestValidate(input);
+
+            result.ShouldHaveValidationErrorFor(d => d.EndDate);
+        }
     }
 }
